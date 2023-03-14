16515d308d8d4a5114b53b2df9883d3a90c87b35
Name <email@example.ru>
2023-03-14 18:47:04 +0300

    move to pets home

--- exemple1.py
+++ exemple1.py
@@ -7,11 +7,11 @@ from selenium.webdriver.common.keys import Keys
 
 from time import sleep
 
-driver = webdriver.Chrome("C/Users/358/project/test_gid/SkillFactory/python_selenium_sf/chromedriver") # Чтобы запустить браузер нужна эта команда. При этом к Chrome нужно подключить определенную вещь.
-driver.get("http://google.com")
-driver.find_element(By.XPATH, "//input[@title=\"Поиск\"]").send_keys('SkillFactory' + Keys.RETURN)
+driver = webdriver.Chrome("C/Users/358/project/test_gid/SkillFactory/python_selenium_sf/chromedriver")
+driver.get("https://130.193.37.179/app/pets")
+t = (driver.find_elements(By.XPATH, "//*[@id=\"image\"]/img"))[0].clic()
+print(t)
 
-driver.save_screenshot('sf.png')
 sleep(3)
 # driver.save_screenshot('pet_home987654e3.png')
 # sleep(100)
